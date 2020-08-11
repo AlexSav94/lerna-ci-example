03c14546f6776048039d5d2ace522066dd7d84ba
AlexSav94 <aleksoider@gmail.com>
2020-08-11 16:22:46 +0300

    feat: not necessary parameters BREAKING CHANGE: empty parameters are allowed

--- packages/string-alert/src/index.ts
+++ packages/string-alert/src/index.ts
@@ -1,7 +1,7 @@
-export function log(string: string) {
+export function log(string?: string) {
   console.log(string)
 }
 
-export function alert(string: string) {
+export function alert(string?: string) {
   window.alert(string)
 }
