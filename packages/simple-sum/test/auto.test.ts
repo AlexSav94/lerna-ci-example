13ff74fa80125004bb791fdc2378db541c980313
AlexSav94 <aleksoider@gmail.com>
2020-08-12 14:39:22 +0300

    feat: simple-sum

---
+++ packages/simple-sum/test/auto.test.ts
@@ -0,0 +1,9 @@
+import { sum, concat } from "../src/index"
+
+test("sum test", () => {
+  expect(sum(3, 3)).toBe(6)
+})
+
+test("concat test", () => {
+  expect(concat("test ", "string")).toBe("test string")
+})
