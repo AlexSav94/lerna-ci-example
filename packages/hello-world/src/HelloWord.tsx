f6d185e4536a8104cf18f6b6704861fa212d86df
AlexSav94 <aleksoider@gmail.com>
2020-08-20 22:39:29 +0300

    feat: red hello world

--- packages/hello-world/src/HelloWord.tsx
+++ packages/hello-world/src/HelloWord.tsx
@@ -3,3 +3,7 @@ import React from "react"
 export function HelloWorld() {
   return <div>Hello World, people</div>
 }
+
+export function HelloWorldRed() {
+  return <div style={{ color: "red" }}>Hello World, people</div>
+}
